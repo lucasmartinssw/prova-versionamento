9d414b4d10f3a3e9bbd24c41ca3b42e64d61895f
Lucas <lucasrmartins40@gmail.com>
2025-07-02 19:19:47 -0300

    feature: criação da página de contato

---
+++ src/app/contato/page.jsx
@@ -0,0 +1,7 @@
+export default function Contato() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-[36px] text-slate-800 font-bold text-center">Seja bem-vindo à página de Contato</h1>
+    </div>
+  )
+}
