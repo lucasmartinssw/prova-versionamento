107361aa25d4381e03a17667f396f1538fc48754
Lucas <lucasrmartins40@gmail.com>
2025-07-02 19:12:38 -0300

    feature: criação da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <div className="flex flex-col gap-5 items-center">
+        <h1 className="text-[36px] text-gray-800 font-bold m-0">Prova Versionamento - RPV Senai Cataguases</h1>
+        <p className="text-[24px] text-slate-500 m-0">Lucas Ramos Martins</p>
+        <p className="text-[24px] text-slate-400 m-0">Cataguases, 02 de Julho de 2025</p>
+      </div>
+    </div>
   )
 }
